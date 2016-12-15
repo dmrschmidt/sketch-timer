65231df9b4d1f4a52f79459fa33493af18501c6f
Dennis Schmidt <dschmidt@pivotal.io>
2016-12-15 14:05:10 +0100

    1st protoype

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,29 @@
+var audio = new Audio('resources/track_001.mp3')
+var countdown_time = 420
 
+function secondsTimeSpanToHMS(s) {
+    var h = Math.floor(s / 3600)
+    s -= h * 3600
+    var m = Math.floor(s / 60)
+    s -= m * 60
+    return m + ":" + (s < 10 ? '0' + s : s)
+}
+
+$('#timer').html(secondsTimeSpanToHMS(countdown_time))
+
+var updateTimer = function() {
+  countdown_time -= 1;
+  $('#timer').html(secondsTimeSpanToHMS(countdown_time))
+}
+
+var timer = null
+
+$('html').click(function() {
+  if(audio.paused) {
+    audio.play()
+    timer = setInterval(updateTimer, 1000)
+  } else {
+    audio.pause()
+    clearInterval(timer)
+  }
+})
