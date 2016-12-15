cc7679629a7e27f8caafbee39a8e8f45cd1ee3ee
Dennis Schmidt <dschmidt@pivotal.io>
2016-12-15 16:42:39 +0100

    reset and vertical centering

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,5 +1,7 @@
 var audio = new Audio('resources/track_001.mp3')
-var countdown_time = 420
+var sketching_duration = 420
+var countdown_time = sketching_duration
+var timer = null
 
 audio.preload = "auto"
 
@@ -16,11 +18,24 @@ $('#timer').html(secondsTimeSpanToHMS(countdown_time))
 var updateTimer = function() {
   countdown_time -= 1;
   $('#timer').html(secondsTimeSpanToHMS(countdown_time))
+
+  if (countdown_time == 0) {
+    audio.pause()
+    clearInterval(timer)
+  }
 }
 
-var timer = null
+var reset = function() {
+  clearInterval(timer)
+  audio.pause()
+  audio.currentTime = 0
+  countdown_time = sketching_duration
+  $('#timer').html(secondsTimeSpanToHMS(countdown_time))
+}
 
 $('html').click(function() {
+  if(countdown_time == 0) return
+
   if(audio.paused) {
     audio.play()
     timer = setInterval(updateTimer, 1000)
@@ -28,4 +43,8 @@ $('html').click(function() {
     audio.pause()
     clearInterval(timer)
   }
+})
+
+$('html').dblclick(function() {
+  reset()
 })
